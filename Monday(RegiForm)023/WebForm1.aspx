22c25767f8d4b6906d32c283d7ac9d57bbcea26d
Santosh Satpute <santoshsatpute1996@gmail.com>
2024-01-19 14:38:35 +0530

    commit/createrepo

---
+++ Monday(RegiForm)023/WebForm1.aspx
@@ -0,0 +1,120 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Monday_RegiForm_023.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+            height: 423px;
+        }
+        .auto-style2 {
+            width: 100%;
+            height: 581px;
+        }
+        .auto-style3 {
+            width: 100%;
+            height: 580px;
+        }
+        .auto-style4 {
+            height: 406px;
+            text-align: center;
+        }
+        .auto-style5 {
+            text-align: center;
+        }
+        .auto-style6 {
+            font-weight: bold;
+            color: #FF6600;
+            margin-left: 0px;
+            background-color: #FFFF00;
+        }
+        .auto-style7 {
+            color: #FF6600;
+            background-color: #FFFF00;
+        }
+        .auto-style8 {
+            color: #CC3399;
+            font-weight: bold;
+            background-color: #FF6600;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="XX-Large" Text="Registretion Form"></asp:Label>
+            <br />
+            <br />
+        </div>
+        <table class="auto-style1">
+            <tr>
+                <td>
+                    <table border="1" class="auto-style2">
+                        <tr>
+                            <td class="auto-style5">Roll No</td>
+                            <td class="auto-style5">
+                                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Search" />
+                                <br />
+                                <asp:Label ID="Label2" runat="server"></asp:Label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="auto-style5">Student&nbsp; Name</td>
+                            <td class="auto-style5">
+                                <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="auto-style5">Father Name</td>
+                            <td class="auto-style5">
+                                <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="auto-style5">Last Name</td>
+                            <td class="auto-style5">
+                                <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="auto-style5" colspan="2">
+                                <strong>
+                                <asp:Button ID="Button3" runat="server" CssClass="auto-style6" OnClick="Button3_Click" Text="Save" Width="100px" />
+                                </strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>
+                                <asp:Button ID="Button2" runat="server" Height="25px" OnClick="Button2_Click" Text="Update" Width="126px" CssClass="auto-style7" />
+                                <br />
+                                <br />
+                                </strong>
+                                <br />
+                                <asp:Label ID="Label3" runat="server"></asp:Label>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td>
+                    <table border="1" class="auto-style3">
+                        <tr>
+                            <td class="auto-style4">
+                                <asp:Image ID="Image1" runat="server" Width="101px" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="auto-style5">
+                                <strong>
+                                <asp:FileUpload ID="FileUpload1" runat="server" CssClass="auto-style8" />
+                                </strong>
+                                <br />
+                                Maximum Size 200 Kb</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ Monday(RegiForm)023/WebForm1.aspx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Monday_RegiForm_023
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        static String imagelink;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String imagelink1;
+            String mycon = @"data source=DESKTOP-A8OGLN5\SQLEXPRESS;initial catalog=Santosh;integrated security=true";
+            String myquery = "Select * from StudentRegi where Student_ID =" + TextBox2.Text;
+            SqlConnection con = new SqlConnection(mycon);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = myquery;
+            cmd.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                Label2.Text = "Particular Roll Number Found";
+                TextBox3.Text = ds.Tables[0].Rows[0]["StudentName"].ToString();
+                TextBox4.Text = ds.Tables[0].Rows[0]["FatherName"].ToString();
+                TextBox5.Text = ds.Tables[0].Rows[0]["LastName"].ToString();
+                imagelink1 = ds.Tables[0].Rows[0]["profileimage"].ToString();
+                Image1.ImageUrl = imagelink1 + "?n=" + DateTime.Now.Second.ToString();
+            }
+            else
+            {
+                Label3.Text = "Particular Roll Number Not Found";
+
+            }
+            con.Close();
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile == true)
+            {
+                if (uploadimage() == true)
+                {
+                    String mycon = @"Data Source=DESKTOP-A8OGLN5\SQLEXPRESS;Initial Catalog=Santosh;Integrated Security=True";
+                    String updatedata = "Update StudentRegi set StudentName='" + TextBox3.Text + "', FatherName='" + TextBox4.Text + "', LastName='" + TextBox5.Text + "', profileimage='" + imagelink + "' where rollno=" + TextBox2.Text;
+                    SqlConnection con = new SqlConnection(mycon);
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = updatedata;
+                    cmd.Connection = con;
+                    cmd.ExecuteNonQuery();
+                    Label3.Text = "Record Has Been Updated Successfully";
+                }
+            }
+            else
+            {
+                String mycon = @"Data Source=DESKTOP-A8OGLN5\SQLEXPRESS;Initial Catalog=Santosh;Integrated Security=True";
+                String updatedata = "Update StudentRegi set StudentName='" + TextBox3.Text + "', FatherName='" + TextBox4.Text + "', LastName='" + TextBox5.Text + "' where Student_ID=" + TextBox2.Text;
+                SqlConnection con = new SqlConnection(mycon);
+                con.Open();
+                SqlCommand cmd = new SqlCommand(); 
+                cmd.CommandText = updatedata;
+                cmd.Connection = con;
+                cmd.ExecuteNonQuery();
+                Label3.Text = "Record Has Been Updated Successfully";
+            }
+        }
+
+        private Boolean uploadimage()
+        {
+            Boolean imagesaved = false;
+            if (FileUpload1.HasFile == true)
+            {
+
+                String contenttype = FileUpload1.PostedFile.ContentType;
+
+                if (contenttype == "image/jpeg")
+                {
+                    int filesize;
+                    filesize = FileUpload1.PostedFile.ContentLength;
+
+                    if (filesize <= 51200)
+                    {
+                        System.Drawing.Image img = System.Drawing.Image.FromStream(FileUpload1.PostedFile.InputStream);
+                        int height = img.Height;
+                        int width = img.Width;
+                        if (height == 200 && width == 200)
+                        {
+                            FileUpload1.SaveAs(Server.MapPath("~/ProfileImage/") + TextBox2.Text + ".jpg");
+                            Image1.ImageUrl = "~/ProfileImage/" + TextBox2.Text + ".jpg";
+                            imagelink = "ProfileImage/" + TextBox2.Text + ".jpg";
+                            imagesaved = true;
+                        }
+                        else
+                        {
+                            Label3.Text = "Kindly Upload JPEG Image in Proper Dimensions 200 x 200";
+                        }
+
+
+
+
+                    }
+                    else
+                    {
+                        Label3.Text = "File Size exceeds 50 KB - Please Upload File Size Maximum 50 KB";
+                    }
+
+                }
+                else
+                {
+                    Label3.Text = "Only JPEG/JPG Image File Acceptable - Please Upload Image File Again";
+                }
+            }
+            else
+            {
+                Label3.Text = "You have not selected any file - Browse and Select File First";
+            }
+
+            return imagesaved;
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            String mycon = @"Data Source=DESKTOP-A8OGLN5\SQLEXPRESS;Initial Catalog=Santosh;Integrated Security=True";
+            String savedata = "insert into StudentRegi StudentName='" + TextBox3.Text + "', FatherName='" + TextBox4.Text + "', LastName='" + TextBox5.Text + "', profileimage='" + imagelink;   // + "' where rollno=" + TextBox2.Text; 
+            SqlConnection con = new SqlConnection(mycon);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = savedata;
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            Label3.Text = "Record Has Been Updated Successfully";
+        }
+    }
+
+}
